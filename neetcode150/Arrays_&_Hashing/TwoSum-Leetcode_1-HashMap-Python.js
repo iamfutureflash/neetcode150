2b4d5a14d2ccb2ab4755b96528ee0e07b14a14b7
iamfutureflash <xenode2000@gmail.com>
2024-03-28 00:32:28 +0530

    updated two sum with new solution

--- neetcode150/Arrays_&_Hashing/TwoSum-Leetcode_1-HashMap-Python.js
+++ neetcode150/Arrays_&_Hashing/TwoSum-Leetcode_1-HashMap-Python.js
@@ -5,17 +5,28 @@ console.log('Two sum');
  * @param {number} target
  * @return {number[]}
  */
+// const twoSum = (nums, target) => {
+//     for (let i = 0; i < nums.length; i++) {
+//         const diff = target - nums[i];
+//         const j = nums.indexOf(diff,i+1);
+//         console.log(nums.indexOf(diff) !== -1);
+//         console.log(nums.indexOf(diff) !== i);
+//         console.log('diff ', diff);
+
+//         console.log('j ',j);
+//         if (j !== -1 ) {
+//             return [i, j]
+//         }
+//     }
+// }
 const twoSum = (nums, target) => {
-    let prevMap = new Object();
-    for (let [i, num] of nums.entries()) {
-        const diff = target - num;
-        if (diff in prevMap) {
-            return [prevMap[diff], i]
-        }
-        prevMap[nums[i]] = i;
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        const j = nums.indexOf(diff,i+1);
+        if (j !== -1 ) {return [i, j]}
     }
-};
-const n = [2, 7, 11, 15];
-const t = 9;
+}
+const n = [4,6,7,8,9];
+const t = 13;
 const r = twoSum(n, t)
-console.log(r);
+console.log('result ', r);
