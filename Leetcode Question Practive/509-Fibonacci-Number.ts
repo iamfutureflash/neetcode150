fcc0c5a5770d5c9088c7e12762a7c4c48e2296c6
PUNEET VAISHNAV <65516671+iamfutureflash@users.noreply.github.com>
2024-09-02 19:36:13 +0530

    Rank 12,95,525 --> 509-Fibonacci-Number.ts / 02/09/2024 - - done with 1 approaches

--- Leetcode Question Practive/509-Fibonacci-Number.ts
+++ Leetcode Question Practive/509-Fibonacci-Number.ts
@@ -11,8 +11,20 @@ const solveFib = (n: number): number => {
 function fib(n: number): number {
     return solveFib(n);
 };
+function fib2(n: number): number {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    let a = 0, b = 1, c = a + b;
+    for (let i = 2; i < n + 1; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+};
 
 console.log(fib(2));
 console.log(fib(3));
 console.log(fib(4));
 console.log(fib(30));
+console.log(fib2(30));
